d698ad484be7beee60990f85cfb4d20f2cf30c97
YanaTyzik <denisyuk.yanina@mail.ru>
2025-03-27 11:17:38 +0300

    Добавьте файлы проекта.

---
+++ MyMVP/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMVP
+{
+    public partial class Form1 : Form, IUsersView
+    {
+        private UsersPresenter presenter_;
+
+        public Form1()
+        {
+            InitializeComponent();
+            presenter_ = new UsersPresenter(new MemoryUsersModel(), this);
+        }
+
+        public void Show(List<User> users)
+        {
+            UsersList.DataSource = null;
+            UsersList.DataSource = users;
+        }
+
+        void NameFiltr_Click(object sender, EventArgs e)
+        {
+            presenter_.View__FiltrEvent(NameTextBox.Text, ComboBoxFiltr.Text);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            presenter_.Model__RefreshLoadedInfoUsers();
+        }
+    }
+}
---
+++ MyMVP/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("MyMVP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMVP")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("93a638c3-6cfa-417c-bbf3-ae375c3d6e65")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
